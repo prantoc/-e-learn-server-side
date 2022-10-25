ffd4972677f7044bf664f09e96aaee82fc433a4b
Pranto Chakraborty <chakrabortypranto39@gmail.com>
2022-10-26 04:37:31 +0600

    API for all courses

--- e-learn/index.js
+++ e-learn/index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
-
+const allCourses = require('./data/generated.json')
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send("Hello this is e-learn platform API ")
+})
+app.get('/courses', (req, res) => {
+    res.send(allCourses)
 })
 
 app.listen(port, () => {
